be9584ed7b6820fa744c564645d01153ca66ebc0
Ali-Balwa77 <akbarabbasbalwa@gmail.com>
2022-10-07 15:36:31 +0530

    product route enabled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -144,7 +144,7 @@ function App()
         <main>
           <Container className='mt-3'>
             <Routes>
-              <Route path="product/:slug" element={ <ProductScreen /> } />
+              <Route path="/product/:slug" element={ <ProductScreen /> } />
               <Route path="/cart" element={ <CartScreen /> } />
               <Route path="/search" element={ <SearchScreen /> } />
               <Route path="/signin" element={ <SigninScreen /> } />
